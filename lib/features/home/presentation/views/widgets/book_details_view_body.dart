519b8d86838508aff5fd9d73b3e8c69deec338fe
osaamaahmeed <iiosamaahmed1@gmail.com>
2025-10-03 16:04:09 +0300

    fixing book detials view using custom scroll view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -12,59 +12,74 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
           child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              CustomBookDetailsAppBar(),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.2),
-                child: const CustomBookImage(),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 43, bottom: 6),
-                child: Text(
-                  "The Jungle Book",
-                  style: Styles.textStyle30.copyWith(
-                    fontWeight: FontWeight.w600,
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CustomBookDetailsAppBar(),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: screenWidth * 0.2,
+                        ),
+                        child: const CustomBookImage(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 43, bottom: 6),
+                        child: Text(
+                          "The Jungle Book",
+                          style: Styles.textStyle30.copyWith(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Opacity(
+                        opacity: 0.7,
+                        child: Text(
+                          "Rudyard Kipling",
+                          style: Styles.textStyle18.copyWith(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 18),
+                      const BookRating(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                      const SizedBox(height: 37),
+                      const BookAction(),
+                      const SizedBox(height: 50),
+                    ],
                   ),
                 ),
               ),
-              Opacity(
-                opacity: 0.7,
-                child: Text(
-                  "Rudyard Kipling",
-                  style: Styles.textStyle18.copyWith(
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 18),
-              const BookRating(mainAxisAlignment: MainAxisAlignment.center),
-              const SizedBox(height: 37),
-              const BookAction(),
-              const SizedBox(height: 50),
-              Align(
-                alignment: AlignmentGeometry.centerLeft,
-                child: Text(
-                  "You can also like",
-                  style: Styles.textStyle16.copyWith(
-                    fontWeight: FontWeight.w900,
+              Padding(
+                padding: EdgeInsetsGeometry.only(left: 16),
+                child: Align(
+                  alignment: AlignmentGeometry.centerLeft,
+                  child: Text(
+                    "You can also like",
+                    style: Styles.textStyle16.copyWith(
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
                 ),
               ),
               const SizedBox(height: 16),
+              const CustomBookDetialsListView(),
+              const SizedBox(height: 25),
             ],
           ),
         ),
-        const CustomBookDetialsListView(),
       ],
     );
   }
 }
-
-
