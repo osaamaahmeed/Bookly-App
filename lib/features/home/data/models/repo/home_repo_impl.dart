6dd802474bf549961bad8c22f1a54e10b3ebb76c
osaamaahmeed <iiosamaahmed1@gmail.com>
2025-10-03 23:22:33 +0300

    addding code to first method

--- lib/features/home/data/models/repo/home_repo_impl.dart
+++ lib/features/home/data/models/repo/home_repo_impl.dart
@@ -1,13 +1,25 @@
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/models/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo{
+  final ApiService apiService;
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+   try {
+     var data = await apiService.get(endPoint: 'volumes/Filtering=Free-ebooks&Sorting=newest &q=subject:Programming');
     
-    throw UnimplementedError();
+    List<BookModel> books = [];
+    for (var item in data['items']) {
+      books.add(BookModel.fromJson(item));
+    }
+    return right(books);
+   } catch (e) {
+      return left(ServerFailure());
+   }
   }
 
   @override
