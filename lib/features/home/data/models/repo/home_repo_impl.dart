7de6c61442b0775b4748883e7b006dc1294a247d
osaamaahmeed <iiosamaahmed1@gmail.com>
2025-10-08 06:46:42 +0300

    implementing feach simillar books

--- lib/features/home/data/models/repo/home_repo_impl.dart
+++ lib/features/home/data/models/repo/home_repo_impl.dart
@@ -48,4 +48,25 @@ class HomeRepoImpl implements HomeRepo {
       return left(ServerFailure(errMessage: e.toString()));
     }
   }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> featchSimillarBooks({required String cateogry}) async {
+    try {
+      var data = await apiService.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=relevance',
+      );
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.FromDioException(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
 }
