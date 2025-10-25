03886075fcf6c327e867737349784ea453dc4f54
osaamaahmeed <iiosamaahmed1@gmail.com>
2025-10-25 15:09:24 +0300

    deleted unnecessary imports

--- lib/features/home/presentation/views/widgets/custom_book_image_loading_indecator.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image_loading_indecator.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CustomBookImageLoadingIndecator extends StatelessWidget {
   const CustomBookImageLoadingIndecator({super.key});
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/newest_books_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_book_list_view_bloc_consumer.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/newest_books_list_view_bloc_builder.dart';
